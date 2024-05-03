415e86514fb3fe574f5ea21e695fcef67eda257b
kevinbatard <kevin.batard4064@gmail.com>
2024-05-03 09:18:46 +0200

    refact navbar

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 import logosncf from "../../../../public/SNCFGAMING.png";
-import Link from "next/link";
 import styles from "../../style/header.module.css";
+import Navbar from "./Navbar";
 
 export default function Header() {
   return (
@@ -10,25 +10,7 @@ export default function Header() {
         className={`${styles.backgroundHeader} d-flex align-items-center p-2`}
       >
         <Image src={logosncf} alt="logo sncf gaming" width={125} />
-        <nav>
-          <ul className="d-flex list-unstyled">
-            <li className={`p-4`}>
-              <Link className={`${styles.fontMenu}`} href="/">
-                Accueil
-              </Link>
-            </li>
-            <li className={`p-4`}>
-              <Link className={`${styles.fontMenu}`} href="/programmation">
-                Programmation
-              </Link>
-            </li>
-            <li className={`p-4`}>
-              <Link className={`${styles.fontMenu}`} href="/streamers">
-                Team
-              </Link>
-            </li>
-          </ul>
-        </nav>
+        <Navbar />
         <div className="ms-auto p-4">O</div>
       </div>
       <div className={`${styles.backgroudBarre} w-full mb-4`}></div>
