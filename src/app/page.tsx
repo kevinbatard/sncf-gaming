e268cb42fd322c76370524b8e3f71f6412dc18e0
kevinbatard <kevin.batard4064@gmail.com>
2024-05-07 10:47:05 +0200

    test embed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
-import Header from "./components/header/Header";
+import Header from './components/header/Header';
 
 export default function Home() {
-  return (
-    <>
-      <Header />
-      <main></main>
-    </>
-  );
+    return (
+        <>
+            <Header />
+            <main>
+                <iframe
+                    src="https://player.twitch.tv/?channel=chowh1&parent=www.localhost:3000.fr"
+                    allowFullScreen={true}
+                    height="378"
+                    width="620"
+                ></iframe>
+            </main>
+        </>
+    );
 }
